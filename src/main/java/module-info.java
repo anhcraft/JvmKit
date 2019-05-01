1a6a1bb5a6e2d1988c7b5817e72f81aea19edf24
anhcraft <admin@anhcraft.dev>
2019-05-01 22:43:29 +0700

    update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module dev.anhcraft.jvmkit {
     requires kotlin.stdlib;
 
+    exports dev.anhcraft.jvmkit.builders;
+    exports dev.anhcraft.jvmkit.helpers;
     exports dev.anhcraft.jvmkit.lang;
     exports dev.anhcraft.jvmkit.lang.annotation;
     exports dev.anhcraft.jvmkit.lang.enumeration;
