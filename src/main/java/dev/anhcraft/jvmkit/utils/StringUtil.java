98eef787f8f69a88b932650fbee2f146ade0931f
anhcraft <admin@anhcraft.dev>
2019-05-01 22:41:06 +0700

    add escape/unescape method for string util

--- src/main/java/dev/anhcraft/jvmkit/utils/StringUtil.java
+++ src/main/java/dev/anhcraft/jvmkit/utils/StringUtil.java
@@ -1,7 +1,26 @@
 package dev.anhcraft.jvmkit.utils;
 
+import dev.anhcraft.jvmkit.lang.annotation.NotNull;
+
 /**
  * This class provides utility methods for {@link String} manipulation.
  */
 public class StringUtil {
+    /**
+     * Escapes the given string.
+     * @param str a string
+     * @return escaped string
+     */
+    public static String escape(@NotNull String str){
+        return str.replace("\\", "\\\\").replace("'", "\\'").replace("\0", "\\0").replace("\n", "\\n").replace("\r", "\\r").replace("\"", "\\\"").replace("\\x1a", "\\Z");
+    }
+
+    /**
+     * Unescapes a string.
+     * @param str a string
+     * @return unescaped string
+     */
+    public static String unescape(@NotNull String str){
+        return str.replace("\\\\\\", "\\").replace("\\'", "'").replace("\\0", "\0").replace("\\n", "\n").replace("\\r", "\r").replace("\\\"", "\"").replace("\\Z", "\\xla");
+    }
 }
