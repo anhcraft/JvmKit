fb27db15d8977bc155f8724b67a91b26b7794f43
anhcraft <huynhduyanh123123@gmail.com>
2019-12-24 19:56:36 +0700

    Updated Condition

--- src/main/java/dev/anhcraft/jvmkit/utils/Condition.java
+++ src/main/java/dev/anhcraft/jvmkit/utils/Condition.java
@@ -3,6 +3,7 @@ package dev.anhcraft.jvmkit.utils;
 import dev.anhcraft.jvmkit.lang.IllegalValueException;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * This class provides methods for value validation.
@@ -38,35 +39,16 @@ public class Condition {
     }
 
     /**
-     * Validates whether two given objects are similar.<br>
-     * If both of them are null, they will be recognized as similar.<br>
+     * Validates whether the given expression is true.<br>
      * If the condition is not met, {@link IllegalValueException} will be thrown.
-     * @param objectA first object
-     * @param objectB second object
+     * @param expression expression
+     * @param errorMessageFormat error message format
+     * @param objects objects to be included the error message
      */
-    public static void equal(Object objectA, Object objectB){
-        if(objectA == null && objectB == null) return;
-        if(objectA != null && objectA.equals(objectB)) return;
+    public static void check(boolean expression, String errorMessageFormat, Object... objects) {
+        if(expression) return;
         try {
-            throw new IllegalValueException(null);
-        } catch (IllegalValueException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Validates whether two given objects are similar.<br>
-     * If both of them are null, they will be recognized as similar.<br>
-     * If the condition is not met, {@link IllegalValueException} will be thrown.
-     * @param objectA first object
-     * @param objectB second object
-     * @param errorMessage error message
-     */
-    public static void equal(Object objectA, Object objectB, String errorMessage){
-        if(objectA == null && objectB == null) return;
-        if(objectA != null && objectA.equals(objectB)) return;
-        try {
-            throw new IllegalValueException(errorMessage);
+            throw new IllegalValueException(String.format(errorMessageFormat, objects));
         } catch (IllegalValueException e) {
             e.printStackTrace();
         }
@@ -78,12 +60,7 @@ public class Condition {
      * @param object object
      */
     public static void notNull(Object object){
-        if(object != null) return;
-        try {
-            throw new IllegalValueException(null);
-        } catch (IllegalValueException e) {
-            e.printStackTrace();
-        }
+        check(object != null);
     }
 
     /**
@@ -93,12 +70,7 @@ public class Condition {
      * @param errorMessage error message
      */
     public static void notNull(Object object, String errorMessage){
-        if(object != null) return;
-        try {
-            throw new IllegalValueException(errorMessage);
-        } catch (IllegalValueException e) {
-            e.printStackTrace();
-        }
+        check(object != null, errorMessage);
     }
 
     /**
@@ -109,12 +81,7 @@ public class Condition {
      * @param <T> the element type
      */
     public static <T> void notEmpty(T[] array){
-        if(array != null && array.length > 0) return;
-        try {
-            throw new IllegalValueException(null);
-        } catch (IllegalValueException e) {
-            e.printStackTrace();
-        }
+        check(array != null && array.length > 0);
     }
 
     /**
@@ -126,12 +93,7 @@ public class Condition {
      * @param <T> the element type
      */
     public static <T> void notEmpty(T[] array, String errorMessage){
-        if(array != null && array.length > 0) return;
-        try {
-            throw new IllegalValueException(errorMessage);
-        } catch (IllegalValueException e) {
-            e.printStackTrace();
-        }
+        check(array != null && array.length > 0);
     }
 
     /**
@@ -142,12 +104,18 @@ public class Condition {
      * @param <E> the element type
      */
     public static <E> void notEmpty(Collection<E> collection){
-        if(collection != null && collection.size() > 0) return;
-        try {
-            throw new IllegalValueException(null);
-        } catch (IllegalValueException e) {
-            e.printStackTrace();
-        }
+        check(collection != null && !collection.isEmpty());
+    }
+
+    /**
+     * Validates whether the given iterator is non-empty.<br>
+     * If the iterator is null, it will still be recognized as empty.<br>
+     * If the condition is not met, {@link IllegalValueException} will be thrown.
+     * @param iterator iterator
+     * @param <E> the element type
+     */
+    public static <E> void notEmpty(Iterator<E> iterator){
+        check(iterator != null && iterator.hasNext());
     }
 
     /**
@@ -159,12 +127,19 @@ public class Condition {
      * @param <E> the element type
      */
     public static <E> void notEmpty(Collection<E> collection, String errorMessage){
-        if(collection != null && collection.size() > 0) return;
-        try {
-            throw new IllegalValueException(errorMessage);
-        } catch (IllegalValueException e) {
-            e.printStackTrace();
-        }
+        check(collection != null && !collection.isEmpty(), errorMessage);
+    }
+
+    /**
+     * Validates whether the given iterator is non-empty.<br>
+     * If the iterator is null, it will still be recognized as empty.<br>
+     * If the condition is not met, {@link IllegalValueException} will be thrown.
+     * @param iterator iterator
+     * @param errorMessage error message
+     * @param <E> the element type
+     */
+    public static <E> void notEmpty(Iterator<E> iterator, String errorMessage){
+        check(iterator != null && iterator.hasNext(), errorMessage);
     }
 
     /**
@@ -313,62 +288,5 @@ public class Condition {
      */
     public static <T> void argNotEmpty(String param, Iterable<T> arg){
         if(arg == null || !arg.iterator().hasNext()) emptyThrow("param");
-    }
-
-    /**
-     * Validates whether the first given argument is "lower" than the second.<br>
-     * If the condition is not met, {@link IllegalArgumentException} will be thrown with the following message:<br>
-     * <em>&#96;&lt;first param&gt;&#96; must be lower than &#96;&lt;second param&gt;&#96; (&lt;previous class&gt;#&lt;previous method&gt;)</em>
-     * @param firstParam the first parameter which the first argument was passed into
-     * @param secondParam the second parameter which the second argument was passed into
-     * @param firstArg the first argument
-     * @param secondArg the second argument
-     */
-    public static void argLower(String firstParam, String secondParam, Object firstArg, Object secondArg){
-        if(ObjectUtil.deepCompare(firstArg, secondArg) < 0) return;
-        StackTraceElement stacktrace = Thread.currentThread().getStackTrace()[2];
-        try {
-            throw new IllegalArgumentException("`"+firstParam+"` must be lower than `"+secondParam+"` ("+stacktrace.getClassName()+"#"+stacktrace.getMethodName()+")");
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Validates whether the first given argument equals the second.<br>
-     * If the condition is not met, {@link IllegalArgumentException} will be thrown with the following message:<br>
-     * <em>&#96;&lt;first param&gt;&#96; must equal &#96;&lt;second param&gt;&#96; (&lt;previous class&gt;#&lt;previous method&gt;)</em>
-     * @param firstParam the first parameter which the first argument was passed into
-     * @param secondParam the second parameter which the second argument was passed into
-     * @param firstArg the first argument
-     * @param secondArg the second argument
-     */
-    public static void argEqual(String firstParam, String secondParam, Object firstArg, Object secondArg){
-        if(ObjectUtil.deepCompare(firstArg, secondArg) == 0) return;
-        StackTraceElement stacktrace = Thread.currentThread().getStackTrace()[2];
-        try {
-            throw new IllegalArgumentException("`"+firstParam+"` must equal `"+secondParam+"` ("+stacktrace.getClassName()+"#"+stacktrace.getMethodName()+")");
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Validates whether the first given argument is "higher" than the second.<br>
-     * If the condition is not met, {@link IllegalArgumentException} will be thrown with the following message:<br>
-     * <em>&#96;&lt;first param&gt;&#96; must be higher than &#96;&lt;second param&gt;&#96; (&lt;previous class&gt;#&lt;previous method&gt;)</em>
-     * @param firstParam the first parameter which the first argument was passed into
-     * @param secondParam the second parameter which the second argument was passed into
-     * @param firstArg the first argument
-     * @param secondArg the second argument
-     */
-    public static void argHigher(String firstParam, String secondParam, Object firstArg, Object secondArg){
-        if(ObjectUtil.deepCompare(firstArg, secondArg) > 0) return;
-        StackTraceElement stacktrace = Thread.currentThread().getStackTrace()[2];
-        try {
-            throw new IllegalArgumentException("`"+firstParam+"` must be higher than `"+secondParam+"` ("+stacktrace.getClassName()+"#"+stacktrace.getMethodName()+")");
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        }
     }
 }
